469372e29be3bb322853d30e98caea1488024239
monxiaolee <li_meng_xiao@yeah.net>
2018-11-16 01:02:16 +0800

    liucheng

--- src/view/topo/index.vue
+++ src/view/topo/index.vue
@@ -15,14 +15,25 @@ export default {
   mounted() {
       let myDiagram = $(go.Diagram, "myDiagramDiv", {
         initialContentAlignment: go.Spot.Center, // 居中显示Diagram内容
-        "undoManager.isEnabled": true // 打开Ctrl-Z撤销和Ctrl-Y重做功能
+        "undoManager.isEnabled": true, // 打开Ctrl-Z撤销和Ctrl-Y重做功能
+        layout: $(go.TreeLayout, { angle: 90, layerSpacing: 35 })
       })
 
-      let myModel = $(go.Model)
+      myDiagram.nodeTemplate = $(go.Node, "Horizontal", 
+      { background: "#44CCFF"},
+      $(go.Picture, { margin: 10, width: 50, height: 50, background: "red" }, new go.Binding("source")),
+      $(go.TextBlock, "默认文本", { margin: 12, stroke: "shite", font: "bold 16px sans-serif" }, new go.Binding("text", "name"))
+      )
+
+      let myModel = $(go.GraphLinksModel)
       myModel.nodeDataArray = [
-          {key: "Alpha"},
-          {key: "spring"},
-          {key: "Gamma"}
+          {key: "Alpha", source: ""},
+          {key: "Spring", source: ""},
+          {key: "Gamma", source: ""}
+      ]
+      myModel.linkDataArray = [
+          {from: "Alpha", to: "Spring"},
+          {from: "Spring", to: "Gamma"}
       ]
       myDiagram.model = myModel
   }
